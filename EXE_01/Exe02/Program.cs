6a22207b31c6d535996dfdded3a108e769da3f87
Alexandre3536 <60271214+Alexandre3536@users.noreply.github.com>
2023-03-06 21:14:52 -0300

    primeiro commit

---
+++ EXE_01/Exe02/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Olá, seja bem vindo");
+Console.WriteLine("Qual é o seu nome?");
+string name = Console.ReadLine();
+Console.WriteLine(
+    $"Olá {name}  prazer conhecê-lo."
+    );
+    
