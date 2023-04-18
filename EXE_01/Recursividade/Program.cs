668e246adf41c1d9b6d98d9add2e094e90d04d42
Alexandre3536 <60271214+Alexandre3536@users.noreply.github.com>
2023-04-17 22:06:38 -0300

    Recursividade

---
+++ EXE_01/Recursividade/Program.cs
@@ -0,0 +1,47 @@
+﻿Console.WriteLine("Cálculo Fatorial Recursivo");
+Console.WriteLine("*********************************************");
+Console.WriteLine();
+
+Console.WriteLine("Digite um número:");
+int numero = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Fatorial de {numero} é {Factorial(numero)}");
+Console.WriteLine($"Fibonacci de {numero} é {Fibonacci(numero)}");
+
+
+static int Factorial(int number)
+{
+    //caso base 1
+    if(number < 0)
+    {
+        throw new ArgumentException(
+            message: $"A função fatorial não suporta números negativos. Input {number}",
+            paramName: nameof(number)
+        );
+    }
+    else if(number == 0) //caso base 2
+    {
+        return 1;
+    }
+    else // Caso Recursivo
+    {
+        return number * Factorial(number -1);
+    }
+}
+
+static long Fibonacci(int n)
+{
+    if( n <= 2L) // Caso base
+        return 1L;
+
+    return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+static void MultiplicarWhile(int n)
+{
+    int fator = 10;
+    while(fator <=10)
+    {
+        Console.WriteLine($"{n} x {fator} = {n * fator}");
+        fator++;
+    }
+}
---
+++ EXE_01/TabuadaDoNove/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program {
+    static void Main (string[] args){
+        int num = 9;
+        Tabuada(num, 0);
+    }
+    static void Tabuada (int num, int i ) {
+        if (i<=10) {
+             Console.WriteLine("{0} x {1} = {2}", num, i, num*i);
+             Tabuada(num, i+1);    
+    }
+}
+}
+
